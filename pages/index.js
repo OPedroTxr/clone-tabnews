50b6ae8d82f77f4c7048683043e3a6dca60c426c
PedroTxr <83319342+OPedroTxr@users.noreply.github.com>
2026-02-13 19:57:58 -0300

    alteração da mensagem exibida no index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home(){
-    return <h1>Teste</h1>
+    return <body>
+            <h1 class="titulo">Oi meu amor!</h1>
+            <h2>Esta é uma mensagem especial para você!</h2>
+            <p>Ainda tô aprendendo como programar em javascript, mas até o final do ano vou ter um projeto pra deixar a gente rico!</p>
+        </body>
+    
 }
 
 export default Home;
