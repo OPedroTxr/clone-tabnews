88581dbd03a40e8341292ce2b9704bb4e2592553
PedroTxr <83319342+OPedroTxr@users.noreply.github.com>
2026-02-15 23:29:37 -0300

    adiciona scripts de `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-function Home(){
-    return <body>
-            <h1 class="titulo">Oi meu amor!</h1>
-            <h2>Esta é uma mensagem especial para você!</h2>
-            <p>Ainda tô aprendendo como programar em javascript, mas até o final do ano vou ter um projeto pra deixar a gente rico!</p>
-        </body>
-    
+function Home() {
+  return (
+    <body>
+      <h1 class="titulo">Oi meu amor!</h1>
+      <h2>Esta é uma mensagem especial para você!</h2>
+      <p>
+        Ainda tô aprendendo como programar em javascript, mas até o final do ano
+        vou ter um projeto pra deixar a gente rico!
+      </p>
+    </body>
+  );
 }
 
 export default Home;
